8ebc4045b4fc2fd9ef54f858ce1df72d126a36c1
pupie <pupiecn@gmail.com>
2017-08-21 21:58:18 +0800

    init commit

---
+++ lede_build_patch.sh
@@ -0,0 +1,4 @@
+sed -i "s/0x7c0000/0xf80000/g" `grep -rl "0x7c0000" ../lede/tools/firmware-utils/src/mktplinkfw.c`
+sed -i "s/0x3c0000/0xf80000/g" `grep -rl "0x3c0000" ../lede/tools/firmware-utils/src/mktplinkfw.c`
+cp ./999-openvpn-2.4.0-xor-patch.patch ../lede/package/network/services/openvpn/patches/
+sed -i '/x509_username_field/a\scramble' ../lede/package/network/services/openvpn/files/openvpn.options
